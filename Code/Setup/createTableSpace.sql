fc2b0bea40f860453bf9d08a9b292832c05a3f0e
Kirby Sand <kirby.sand@hp.com>
2017-11-19 21:02:31 -0800

    saving

--- Code/Setup/createTableSpace.sql
+++ Code/Setup/createTableSpace.sql
@@ -4,8 +4,12 @@
 alter session set container=cdb$root;
 alter system set db_16k_cache_size=20M;
 alter system set db_32k_cache_size=20M;
-alter session set container=pdb1;
-create tablespace cap8k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap8k.dbf' size 1g;
-create tablespace cap16k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap16k.dbf' size 1g blocksize 16k;
-create tablespace cap32k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap32k.dbf' size 1g blocksize 32k;
+alter session set container=capstone;
+create tablespace cap8k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap8k.dbf' size 1g extent management local uniform size 40k;
+create tablespace cap16k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap16k.dbf' size 1g blocksize 16k extent management local uniform size 80k;
+create tablespace cap32k datafile '/u01/app/oracle/oradata/cdb1/pdb1/cap32k.dbf' size 1g blocksize 32k extent management local uniform size 160k;
 select * from DBA_DATA_FILES;
+
+DROP TABLESPACE CAP8K INCLUDING CONTENTS;
+DROP TABLESPACE CAP16K INCLUDING CONTENTS;
+DROP TABLESPACE CAP32K INCLUDING CONTENTS;
