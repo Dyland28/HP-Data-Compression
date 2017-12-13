d97c169cae607f8965dc2c786e97c9f80adf04d0
Kirby Sand <kirby.sand@hp.com>
2017-12-13 15:06:48 -0800

    saving

--- Kirby/ROWS_PER_BLOCK.sql
+++ Kirby/ROWS_PER_BLOCK.sql
@@ -1,11 +1,34 @@
-select
-   dbms_rowid.rowid_relative_fno(rowid) REL_FNO,
-   dbms_rowid.rowid_block_number(rowid) BLOCKNO,
---   dbms_rowid.rowid_row_number(rowid) ROWNO
-   COUNT(*)
-   from capstone.SIZE_MATH_TEST
-   GROUP BY
+TRUNCATE TABLE CAPSTONE.ROWS_PER_BLOCK;
+DROP TABLE CAPSTONE.ROWS_PER_BLOCK;
+CREATE TABLE CAPSTONE.ROWS_PER_BLOCK
+(
+OWNER VARCHAR2(30),
+TABLE_NAME VARCHAR2(30),
+REL_FNO NUMBER,
+BLOCKNO NUMBER,
+TOTAL_ROWS NUMBER
+);
+
+INSERT INTO CAPSTONE.BLOCKS_PER_ROW
+
+SELECT
+    :OWNER,
+    :TABLE_NAME,
+    dbms_rowid.rowid_relative_fno(rowid) REL_FNO,
+    dbms_rowid.rowid_block_number(rowid) BLOCKNO,
+    COUNT(*) AS TOTAL_ROWS
+FROM capstone.SIZE_MATH_TEST_5
+GROUP BY
     dbms_rowid.rowid_relative_fno(rowid),
-   dbms_rowid.rowid_block_number(rowid);
+    dbms_rowid.rowid_block_number(rowid);
+    
+COMMIT;
+
+SELECT
+    OWNER,
+    TABLE_NAME,
+    MAX(TOTAL_ROWS) AS ROWS_PER_BLOCK
+FROM CAPSTONE.
+
    
 ALTER SYSTEM DUMP DATAFILE 12 BLOCK 363;
